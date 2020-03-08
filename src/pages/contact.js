df67fc04843759c5a85835aa2865d3d577ae6ec2
Anchieta Júnior <santosjr87@gmail.com>
2020-03-08 19:26:39 -0300

    Update contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,21 @@ import Layout from '../components/layout'
 const ContactPage = () => {
   return (
     <Layout>
-      <h1>This is a nice contact page</h1>
+      <div className="contact-page">
+        <h1 className="page-title contact-title">Contato</h1>
+
+        <p>Olá, meu nome é Anchieta Júnior, sou um desenvolvedor Ruby e Javascript com 9
+          anos de experiência em desenvolvimento de projetos web.
+        </p>
+        <p>Sou graduado em Sistemas de Informação pela Universidade do Rio São Francisco 
+          (Unirios) e atualmente estou me especializando em Arquitetura de Sistemas Distribuídos.
+        </p>
+
+        <p>Você pode me encontrar pelo <a href="https://twitter.com/anchietajnr" target="_blank">Twitter</a>, 
+          <a href="https://github.com/anchietajunior" target="_blank">Github</a>, 
+          <a href="https://www.linkedin.com/in/anchietajunior/" target="_blank">Linkedin</a> ou 
+          pelo e-mail: santosjr87@gmail.com</p>
+      </div>
     </Layout>
   )
 }
