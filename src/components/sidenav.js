dc06d92191fd391ac940dcea86ba7d496514fb2d
Anchieta Júnior <santosjr87@gmail.com>
2020-03-09 12:56:45 -0300

    Update sidenav

--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -29,6 +29,14 @@ const SideNav = () => {
           <li><a href="https://www.linkedin.com/in/anchietajunior/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a></li>
           <li><a href="https://github.com/anchietajunior" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a></li>
         </ul>
+        <div className="buy-me">
+          <p>Gostou do conteúdo?</p>
+
+          <a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/VCUyWLi">
+            <img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Contribua com um café" />
+            <span style={{fontSize: `1.4em !important`}}>Contribua com um café</span>
+          </a>
+        </div>
       </nav>
     </>
   )
